d0814cc07ef90fe14e7144067eec954d53aebc53
herisusantoarismann <heri.s.arisman@gmail.com>
2022-05-21 13:54:27 +0700

    Add change Imam

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,7 +5,8 @@ import { useParams } from "react-router-dom";
 export const List = () => {
   const params = useParams();
   const [surah, setSurah] = useState(undefined);
-  const [imam, setImam] = useState(undefined);
+  const [imam, setImam] = useState(1);
+  const [imamList, setImamList] = useState(undefined);
   const [imamMenu, setImamMenu] = useState(false);
   const [audio, setAudio] = useState({
     data: new Audio(),
@@ -16,13 +17,15 @@ export const List = () => {
 
   const getImam = () => {
     axios.get(`https://quran-endpoint.vercel.app/imam`).then((res) => {
-      setImam(res.data.data);
+      setImamList(res.data.data);
     });
   };
 
   const getData = () => {
     axios
-      .get(`https://quran-endpoint.vercel.app/quran/${params.surah}`)
+      .get(
+        `https://quran-endpoint.vercel.app/quran/${params.surah}?imamId=${imam}`
+      )
       .then((res) => {
         setSurah(res.data.data);
       });
@@ -57,31 +60,58 @@ export const List = () => {
     audio.data.volume = e.target.value / 100;
   };
 
+  const changeImam = (id) => {
+    setImam(id);
+    stopSound();
+  };
+
   useEffect(() => {
     getData();
     getImam();
   }, []);
 
+  useEffect(() => {
+    getData();
+  }, [imam]);
+
   return (
     <>
-      {imam !== undefined ? (
+      {imamList !== undefined ? (
         <div
           className={`absolute bg-white border-2 border-secondary py-2 px-4 w-11/12 h-[95%] top-2/4 right-2/4 -translate-y-2/4 translate-x-2/4 overflow-y-auto rounded-2xl ${
             imamMenu ? "visible z-20" : "invisible z-0"
           }`}
         >
-          <h1 className="text-center font-bold text-lg">Imam List</h1>
+          <h1 className="text-center font-bold text-lg lg:text-2xl">
+            Imam List
+          </h1>
           <span
-            className="absolute right-2 top-2"
+            className="absolute right-2 top-2 cursor-pointer"
             onClick={() => setImamMenu(false)}
           >
             &#10006;
           </span>
-          {imam.map((item, index) => {
+          {imamList.map((item, index) => {
             return (
-              <div className="my-2 py-1 px-2 flex">
-                <p className="mr-4">{index + 1}.</p>
-                <p>{item.name}</p>
+              <div
+                className={`my-2 py-1 px-2 flex font-semibold ${
+                  imam === index + 1 ? "text-secondary" : ""
+                }`}
+              >
+                <p className="mr-4">
+                  {index + 1} {item.number}.
+                </p>
+                <p
+                  className="mr-2 underline cursor-pointer hover:text-secondary"
+                  onClick={() => changeImam(item.id)}
+                >
+                  {item.name}
+                </p>
+                {imam === index + 1 ? (
+                  <p className="text-black">(Now use this voice)</p>
+                ) : (
+                  ""
+                )}
               </div>
             );
           })}
