7e449ebf9abd7dc1bf2b20c6fa3a609474210b24
herisusantoarismann <heri.s.arisman@gmail.com>
2022-05-21 14:48:45 +0700

    Add sort

--- src/components/ListImam.jsx
+++ src/components/ListImam.jsx
@@ -3,15 +3,17 @@ import axios from "axios";
 import { useOutletContext } from "react-router-dom";
 
 export const ListImam = () => {
-  const [imam, setImam] = useOutletContext();
+  const [imam, setImam] = useState(undefined);
   // type false if ascending and true if descending
   const [sort, setSort] = useState(false);
 
-  const changeSort = () => {
+  const changeSort = (sort) => {
     if (sort) {
-      setImam(imam.sort((a, b) => a.name - b.name));
+      setImam(imam.sort((a, b) => a.name.localeCompare(b.name)));
+      setSort(!sort);
     } else {
-      setImam(imam.sort((a, b) => b.name - a.name));
+      setImam(imam.sort((a, b) => b.name.localeCompare(a.name)));
+      setSort(!sort);
     }
   };
 
@@ -33,7 +35,7 @@ export const ListImam = () => {
         <div className="hidden sm:flex gap-4">
           <div
             className="relative p-2 w-24 bg-white md:w-36 md:p-3 rounded-lg shadow-sm cursor-pointer"
-            onClick={() => changeSort()}
+            onClick={() => changeSort(sort)}
           >
             <div className="leading-6">
               <p className="text-slate-400">Sort by</p>
@@ -51,7 +53,7 @@ export const ListImam = () => {
         <div className="flex justify-end sm:hidden">
           <div
             className="p-1 w-20 flex items-center gap-1 cursor-pointer"
-            onClick={() => changeSort()}
+            onClick={() => changeSort(sort)}
           >
             <p>Alphabet</p>
             {sort ? (
@@ -63,22 +65,28 @@ export const ListImam = () => {
         </div>
       </div>
       <div className="my-2 lg:px-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4">
-        {imam.map((item, index) => {
-          return (
-            <div
-              className="bg-white h-32 xl:h-40 p-2 md:p-4 rounded-md md:rounded-lg flex justify-between flex-col text-sm md:text-base shadow-sm cursor-pointer z-10 duration-300 hover:-translate-x-8 hover:translate-y-3"
-              key={item.id}
-            >
-              <div className="flex justify-between items-center">
-                <span>{index + 1}</span>
-                <i className="fa-solid fa-star text-secondary"></i>
-              </div>
-              <p className="font-semibold text-xs leading-3 lg:text-sm xl:font-bold tracking-wide">
-                {item.name}
-              </p>
-            </div>
-          );
-        })}
+        {imam !== undefined ? (
+          <>
+            {imam.map((item, index) => {
+              return (
+                <div
+                  className="bg-white h-32 xl:h-40 p-2 md:p-4 rounded-md md:rounded-lg flex justify-between flex-col text-sm md:text-base shadow-sm cursor-pointer z-10 duration-300 hover:-translate-x-8 hover:translate-y-3"
+                  key={item.id}
+                >
+                  <div className="flex justify-between items-center">
+                    <span>{index + 1}</span>
+                    <i className="fa-solid fa-star text-secondary"></i>
+                  </div>
+                  <p className="font-semibold text-xs leading-3 lg:text-sm xl:font-bold tracking-wide">
+                    {item.name}
+                  </p>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
