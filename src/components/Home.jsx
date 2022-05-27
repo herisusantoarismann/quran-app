e04e02b63001f42b29639f18d9af85c4728aec9f
herisusantoarismann <heri.s.arisman@gmail.com>
2022-05-27 11:31:26 +0700

    Change Copyright

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -127,7 +127,7 @@ export const Home = () => {
           </div>
         </div>
       </div>
-      <div className="text-center text-xs md:text-sm font-bold text-slate-400 fixed bottom-12 left-2/4 -translate-x-2/4">
+      <div className="text-center text-xs md:text-sm xl:text-base font-bold text-slate-400 fixed bottom-12 lg:bottom-4 xl:bottom-8 left-2/4 -translate-x-2/4">
         &copy; Copyright {new Date().getFullYear()}, All Right Reversed.
       </div>
     </div>
