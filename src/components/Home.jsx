7ce91f7396028ff29b27d668a7fbe4608dc8ebdf
herisusantoarismann <heri.s.arisman@gmail.com>
2022-05-27 13:03:42 +0700

    Update sidebar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -78,10 +78,10 @@ export const Home = () => {
         </div>
       </div>
       <div className="h-5/6 flex flex-col-reverse 2xl:flex-row">
-        <div className="h-full w-11/12 lg:w-10/12 bg-primary my-6 mx-4 lg:ml-24 lg:mr-24 p-4 lg:p-6 rounded-lg lg:rounded-3xl overflow-y-auto">
+        <div className="h-full w-11/12 lg:w-10/12 bg-primary my-6 mx-4 lg:ml-24 lg:mr-24 xl:mr-12 p-4 lg:p-6 rounded-lg lg:rounded-3xl overflow-y-auto">
           <Outlet />
         </div>
-        <div className="hidden 2xl:flex w-1/5 pr-12 py-16 justify-between flex-col">
+        <div className="hidden 2xl:flex w-64 pr-12 py-16 justify-between flex-col">
           <div className="flex justify-between items-center">
             <div className="">
               <h3 className="">Salam,</h3>
