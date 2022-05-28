63735abf85266afe6a5040cf3ecc220c7cbc7117
herisusantoarismann <heri.s.arisman@gmail.com>
2022-05-28 10:47:19 +0700

    Fix Content Quote

--- src/components/Content/ContentQuote.jsx
+++ src/components/Content/ContentQuote.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const ContentQuote = () => {
+export const ContentQuote = ({ quote }) => {
   return (
     <div className="bg-secondary px-6 py-4 text-white rounded-xl font-semibold">
       <h1 className="text-slate-200 opacity-70">Quote of the day</h1>
