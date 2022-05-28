0b9e5c5b9b559e37d69cf82566ef0068bed98f81
herisusantoarismann <heri.s.arisman@gmail.com>
2022-05-28 09:52:21 +0700

    Separate Sidebar Icon Component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import { SidebarIcon } from "./SidebarIcon";
 
 export const Sidebar = () => {
   const location = useLocation();
 
   return (
     <div className="h-full absolute hidden lg:flex p-7 bg-slate-50/50 flex-col rounded-r-xl text-center">
-      <div className="bg-secondary rounded-full p-2">
-        <img src="images/icon.png" alt="" className="w-6 cursor-pointer" />
-      </div>
+      <SidebarIcon />
       <div className="mt-12">
         <Link to={"/"}>
           <i
---
+++ src/components/Sidebar/SidebarIcon.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SidebarIcon = () => {
+  return (
+    <div className="bg-secondary rounded-full p-2">
+      <img src="images/icon.png" alt="" className="w-6 cursor-pointer" />
+    </div>
+  );
+};
