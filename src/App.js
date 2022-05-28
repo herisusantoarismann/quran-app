c23b35ecb870aa281b469b61d00eda11e159b93e
herisusantoarismann <heri.s.arisman@gmail.com>
2022-05-28 11:25:15 +0700

    Separate Surah Container Component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import { List } from "./components/List";
-import { ListBox } from "./components/ListBox";
 import { ImamContainer } from "./components/Content/Imam/ImamContainer";
+import { SurahContainer } from "./components/Content/Surah/SurahContainer";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}>
-          <Route index element={<ListBox />} />
+          <Route index element={<SurahContainer />} />
           <Route path="/:surah" element={<List />} />
           <Route path="/imam" element={<ImamContainer />} />
         </Route>
